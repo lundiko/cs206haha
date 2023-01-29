817613fbbd771710546957ef535850b63b0944d4
lundiko <turtlepetra@icloud.com>
2023-01-29 11:43:23 -0500

    the actual monolith lookin thing yar

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+#size = [ len, w, h ]
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,2,3])
 pyrosim.End()
