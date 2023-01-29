918834f166556d55322bd216030b0f09c21ad114
lundiko <turtlepetra@icloud.com>
2023-01-29 11:50:47 -0500

    boxes 2 yar

--- generate.py
+++ generate.py
@@ -1,6 +1,10 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
+x = 0
+y = 0
+z = .5
 #size = [ len, w, h ]
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,2,3])
+pyrosim.Send_Cube(name="Box", pos=[x,y,z,.5] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box2", pos=[x+1,y,z+1,.5] , size=[1,1,1])
 pyrosim.End()
