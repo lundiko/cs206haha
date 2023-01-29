f6b7ca0a8d3df5fc3c7181533e53e756b17fd19c
lundiko <turtlepetra@icloud.com>
2023-01-29 12:52:23 -0500

    file with the robot parts yar

--- generate.py
+++ generate.py
@@ -1,10 +1,20 @@
 import pyrosim.pyrosim as pyrosim
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-4,4,.5,.5] , size=[1,1,1])
+    pyrosim.End()
 
-pyrosim.Start_SDF("boxes.sdf")
-x = 0
-y = 0
-z = .5
-#size = [ len, w, h ]
-pyrosim.Send_Cube(name="Box", pos=[x,y,z,.5] , size=[1,1,1])
-pyrosim.Send_Cube(name="Box2", pos=[x+1,y,z+1,.5] , size=[1,1,1])
-pyrosim.End()
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,1.5] , size=[1,1,1])
+    pyrosim.Send_Joint( name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-1,0,-.5] , size=[1,1,1])
+    
+    pyrosim.Send_Joint( name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[1,0,-.5] , size=[1,1,1])
+    
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
