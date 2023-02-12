70fdd051526009fc6f749300e9039b1227caf4a0
lundiko <turtlepetra@icloud.com>
2023-02-12 12:29:58 -0500

    graph now has axis titles yar!

--- analyze.py
+++ analyze.py
@@ -1,9 +1,12 @@
 import numpy as numpy
 import matplotlib.pyplot
 
+
 backLegSensorValues = numpy.load("data/backData.npy")
 frontLegSensorValues = numpy.load("data/frontData.npy")
 matplotlib.pyplot.plot(backLegSensorValues, label ='back leg', linewidth =4 )
 matplotlib.pyplot.plot(frontLegSensorValues, label ='front leg' )
+matplotlib.pyplot.xlabel('Steps')
+matplotlib.pyplot.ylabel('Value in Radians')
 matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
